3e16d491dde6393453032ad534fd5246a57cf58e
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2022-05-13 14:12:41 +0100

    Added function declaration to tic_tac_toe.h header file

--- include/tic_tac_toe.h
+++ include/tic_tac_toe.h
@@ -2,5 +2,10 @@
 
 namespace hayohtee
 {
-    
+    void clear();
+    void print_board(const char arr[]);
+    int check_win(const char arr[]);
+    int prompt(int player);
+    bool update_board(char arr[], char mark, int choice);
+
 }
